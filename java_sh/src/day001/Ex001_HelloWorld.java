c27acf70fe507795b8bd73a9befc34649a4ffd5f
Soon17 <acejsh77@naver.com>
2024-12-30 12:42:27 +0900

    1일차 학습내용

---
+++ java_sh/src/day001/Ex001_HelloWorld.java
@@ -0,0 +1,16 @@
+package day001;
+
+public class Ex001_HelloWorld {
+	/**
+	 * 메서드용 특수 주석(main 에 커서 올려보기)
+	 * 
+	 */
+	//프로그램이 실행되려면 main 이 있어야 한다.
+	public static void main(String[] args) {
+		//들여쓰기 Tab
+		//내어쓰기 Shift + Tab
+		System.out.println("Hello World!");
+		
+	}
+
+}
