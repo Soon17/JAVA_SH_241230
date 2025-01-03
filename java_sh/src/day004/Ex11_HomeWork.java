0d513374c41a26cee6e20b5042e5f56fdcd15fea
Soon17 <acejsh77@naver.com>
2025-01-03 17:52:07 +0900

    4일차 수업종료

--- java_sh/src/day004/Ex11_HomeWork.java
+++ java_sh/src/day004/Ex11_HomeWork.java
@@ -1,44 +1,55 @@
 package day004;
 
+import java.util.Scanner;
+
 public class Ex11_HomeWork {
 
 	public static void main(String[] args) {
-		/*다음과 같이 실행되도록 프로그램을 작성하세요.
-		 * *******메뉴*******
-		 * 1.UpDown 게임 실행
-		 * 2. 최고기록 확인
-		 * 3. 프로그램 종료
-		 * ****************
-		 * 메뉴선택: 2
-		 * 게임을 실행한 기록이 없습니다.
-		 * *******메뉴*******
-		 * 1.UpDown 게임 실행
-		 * 2. 최고기록 확인
-		 * 3. 프로그램 종료
-		 * ****************
-		 * 메뉴선택: 1
-		 * 입력: 50
-		 * Down!
-		 * 입력: 25
-		 * 정답!
-		 * *******메뉴*******
-		 * 1.UpDown 게임 실행
-		 * 2. 최고기록 확인
-		 * 3. 프로그램 종료
-		 * ****************
-		 * 메뉴선택: 2
-		 * 2회
-		 * 메뉴로 돌아가려면 엔터를 입력하세요.
-		 * 메뉴
-		 * *******메뉴*******
-		 * 1.UpDown 게임 실행
-		 * 2. 최고기록 확인
-		 * 3. 프로그램 종료
-		 * ****************
-		 * 메뉴선택: 3
-		 * 프로그램을 종료합니다.
-		 * */
-		
+		Scanner sc = new Scanner(System.in);
+		char c;
+		do {
+			
+			printMenu();
+			String choise = sc.next();
+			c = choise.charAt(0);
+			if(choise.length()!=1) continue;
+			switch(c) {
+			case '1': 
+				playGame();
+				exit();
+				break;
+			case '2':
+				showScore();
+				exit();
+				break;
+			case '3':
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			default:
+				System.out.println("없는 메뉴입니다.");
+				break;
+			}
+		} while(c!='3');
+	}
+
+	private static void showScore() {
+		System.out.println("(결과화면)");
+	}
+
+	private static void exit() {
+		System.out.println("(나가기)");
+	}
+
+	private static void playGame() {
+		System.out.println("(게임실행)");
+	}
+
+	private static void printMenu() {
+		System.out.println("*******메뉴*******\n"
+						+ "* 1. UpDown 게임 실행\n"
+						+ "* 2. 최고기록 확인\n"
+						+ "* 3. 프로그램 종료\n"
+						+ "****************\n");
 	}
 
 }
