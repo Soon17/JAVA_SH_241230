94a39660979e76b01f5b842eb041961451beee8e
Soon17 <acejsh77@naver.com>
2025-03-04 13:40:41 +0900

    DB 연동

---
+++ java_sh/src/db/ex1/model/vo/ScoreVO.java
@@ -0,0 +1,15 @@
+package db.ex1.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ScoreVO {
+	
+	private int sc_num;
+	private int sc_st_key;
+	private int sc_sj_num;
+	private int sc_score;
+	
+}
---
+++ java_sh/src/db/ex1/service/StudentServiceImp.java
@@ -0,0 +1,50 @@
+package db.ex1.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import db.ex1.dao.StudentDAO;
+import db.ex1.model.vo.StudentVO;
+
+public class StudentServiceImp implements StudentService {
+
+	private StudentDAO studentDao;
+	
+	public StudentServiceImp() {
+		String resource = "db/ex1/config/mybatis-config.xml";
+		InputStream inputStream;
+		SqlSession session;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			session = sessionFactory.openSession(true); //true : insert, update, delete 등의 commit 역할
+			studentDao = session.getMapper(StudentDAO.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public List<StudentVO> getStudentList() {
+		return studentDao.selectStudentList();
+	}
+
+	@Override
+	public StudentVO getStudent(int grade, int classNum, int num) {
+		return studentDao.selectStudent(grade, classNum, num);
+	}
+
+	@Override
+	public StudentVO getStudent(StudentVO studentVO) {
+		if(studentVO == null) {
+			return null;
+		}
+		return studentDao.selectStudent2(studentVO);
+	}
+}
