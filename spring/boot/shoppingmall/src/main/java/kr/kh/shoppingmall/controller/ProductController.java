19c211b534642510e47e9d97cc767115aac19e53
Soon17 <acejsh77@naver.com>
2025-04-29 17:31:45 +0900

    제품출력 구현

---
+++ spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/ProductController.java
@@ -0,0 +1,30 @@
+package kr.kh.shoppingmall.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import kr.kh.shoppingmall.model.vo.ProductVO;
+import kr.kh.shoppingmall.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+	@Autowired
+	ProductService productService;
+
+	@GetMapping("/list/{ca_num}")
+	public String list(@PathVariable int ca_num, Model model) {
+		List<ProductVO> list = productService.getProductList(ca_num);
+		model.addAttribute("productList", list);
+		return "product/list";
+	}
+	
+}
